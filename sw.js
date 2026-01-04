47b4499f0aafa4ed548bf6bae75c0d6f95f6986d
Micfri <44413144+mikefri@users.noreply.github.com>
2026-01-04 08:41:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,4 @@
-const CACHE_NAME = 'ventilo-cache-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/959/959711.png'
-];
-
-// Installation : Mise en cache des fichiers
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Stratégie : Réseau d'abord, sinon Cache
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+  // Ce script permet à l'app de fonctionner même avec une connexion instable
+  event.respondWith(fetch(event.request));
 });
